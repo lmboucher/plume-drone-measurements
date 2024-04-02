e1ec5f38e7fdc1c3780369ba42b75dc8a23630ed
lmboucher <162991823+lmboucher@users.noreply.github.com>
2024-04-02 18:24:10 +0200

    Update GPS_communication_only.py

--- remote_station/GPS_communication_only.py
+++ remote_station/GPS_communication_only.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr  2 17:59:56 2024
-
-@author: lucie
-"""
-
 #--------------------
 #SET THE PARAMETERS :
 #--------------------
