5518847ffb98bcda83399a438235ade2a2649123
lmboucher <162991823+lmboucher@users.noreply.github.com>
2024-04-03 09:44:55 +0200

    Update GPS_communication_only.py

--- remote_station/GPS_communication_only.py
+++ remote_station/GPS_communication_only.py
@@ -5,7 +5,7 @@
 # Set the right serial port name for the Raspberry to look at the right place
 serial_port_name_GPS = '/dev/ttyAMA0'
 #file name where to save the received data from the GPS module
-YOUR_PATH = "home/blabla/blabla/..."
+YOUR_PATH = "/home/blabla/blabla/..."
 log_file_name = YOUR_PATH + "GPS_res.txt"
 # Set the correct baudrate
 baudrate = 9600
@@ -37,25 +37,22 @@ def read_gps_info(gps_ser, file) :
     However, between one line starting with GGA and another one, other lines are sent by the GPS (we do not want them).
     Therefore, when we call this function, it returns the next line starting by GGA. There is a variable offset of some milliseconds 
     between the result and the moment the function is called.
-    '''
+    '''    
+        
+    data = gps_ser.readline() #reading the line sent by the GPS (can start by GGA or something else)
+    start_by_GGA = False #I set this variable assuming the line we read was not starting by GGA
     
-    #necessary condition not to have issues, "with" ensures the serial port is closed after use
-    with gps_ser as serialPort :
+    while not start_by_GGA : #Until our line is not starting by GGA, we read another one
         
-        data = serialPort.readline() #reading the line sent by the GPS (can start by GGA or something else)
-        start_by_GGA = False #I set this variable assuming the line we read was not starting by GGA
+        if data.find(b'GGA') > 0 : #check for bytes-like objects starting by GGA
+            msg = pynmea2.parse(data.decode()) #returns a sentence GGA(...) with the different GPS information
+            start_by_GGA = True # To go out of the loop afterward
+            #We get the different information, here very simply organized
+            my_string = "{0}, {1} {2}, {3} {4}, {5} {6}\n".format(msg.timestamp, msg.latitude, msg.lat_dir, msg.longitude, msg.lon_dir, msg.altitude, msg.altitude_units)
+            print_and_write(file, my_string) 
         
-        while not start_by_GGA : #Until our line is not starting by GGA, we read another one
-            
-            if data.find(b'GGA') > 0 : #check for bytes-like objects starting by GGA
-                msg = pynmea2.parse(data.decode()) #returns a sentence GGA(...) with the different GPS information
-                start_by_GGA = True # To go out of the loop afterward
-                #We get the different information, here very simply organized
-                my_string = "{0} {1}, {2} {3}, {4} {5}\n".format(msg.latitude, msg.lat_dir, msg.longitude, msg.lon_dir, msg.altitude, msg.altitude_units)
-                print_and_write(file, my_string) 
-            
-            else :
-                data = serialPort.readline()
+        else :
+            data = gps_ser.readline()
 
 #---------------------------
 #ENTRY POINT OF THE SCRIPT :
@@ -70,3 +67,5 @@ if __name__ == "__main__":
         while True :
             
             read_gps_info(gps_ser, my_file)
+    
+    gps_ser.close()
